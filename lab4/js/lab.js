e9d6da88a765770b129880b5f56b8f07dab92b4a
romariolinares1 <relinare@ucsc.edu>
2023-04-20 18:52:12 -0700

    HTML and Javascript changes made to lab4 files

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock Paper Scissors
+// An implementation of the game
+// Romario Linares <relinare@ucsc.edu>
+// April 2023
 
-// Constants
-
-// Functions
+// Print the options each turn for both players
+  // One user input from both players is accepted: rocks, paper, or scissors
+  // Print countdown as in... 3, 2, 1 or rock, paper, scissor!
+    // If rock and paper...
+      // Print paper wins
+    // Else if rock and scissors...
+      // Print rock wins
+    // Else if paper and scissors...
+      // Print scissors wins
+    // Else if player 1 input = player 2 imput...
+      // Print tie
+        // Give user input again and restart from step 2
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
