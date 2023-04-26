d13e1aeb08888acdf26e912daa7f01ae696f304a
romariolinares1 <relinare@ucsc.edu>
2023-04-26 14:09:04 -0700

    Changes made to Javascript and HTML files

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,18 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Romario Linares <relinare@ucsc.edu>
+// Created: 4.26.2023
 
-// Constants
+// Define Variables
+var make = "Honda";
+var model = "Civic";
+var color = "Chrome";
+var year = 2010;
 
-// Functions
+// Age calculator
+var age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
