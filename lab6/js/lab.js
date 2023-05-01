c84e84daf43e12bd7ed467ed82e4088c47ff340e
romariolinares1 <relinare@ucsc.edu>
2023-05-01 15:59:28 -0700

    Changes made to html, css, and javascript for lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Romario Linares <relinare@ucsc.edu>
+// Date: 1 May 2023
 
-// Constants
+// Define myTransport array
+myTransport = ["Honda Civic", "Metro Bus", "walking"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Creating myMainRide object
+myMainRide = {
+  make: "Honda",
+  model: "Civic",
+  color: "Chrome",
+  year: 2010,
+  // Defining function for age
+  age: function() {
+    return 2023 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Output
+document.writeln("Getting around: " + myTransport + "<br>");
 
-// let's get this party started
-main();
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
