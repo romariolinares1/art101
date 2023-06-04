8fca3d764f87b64705497cc310af6404bf3a8d5c
romariolinares1 <relinare@ucsc.edu>
2023-06-04 15:37:46 -0700

    changes made to html and css and javascript

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,14 +2,17 @@
 // Date: 4 June 2023
 
 $.ajax({
-    url: "https://xkcd.com/info.0.json",
+    url: "https://xkcd.com/614/info.0.json",
     data: {},
     type: "GET",
     dataType: "json",
 
-    // Success function prints comicObj variable in console and 
+    // Success function prints comicObj variable in console and return title and img
     success: function (comicObj) {
       console.log(comicObj);
+      $("#output").html("<p>" + comicObj.title + "</p>");
+      // Gives img the alt attribute by assigning it the alt of the comicObj
+      $("#output").append("<img src =" + comicObj.img + " alt =\"" + comicObj.alt + "\">");
     },
 
     // Check to see if there is an error in API
